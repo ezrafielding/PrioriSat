b42992118313ed9f38e6c2dd746b0ac90c6e9371
Ezra Fielding <ezra.fielding@gmail.com>
2023-12-12 20:06:43 +0900

    Start finetuning YOLO

---
+++ yolo_dota.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Create a new YOLOv8n-OBB model from scratch
+model = YOLO('yolov8n.yaml')
+
+# Train the model on the DOTAv2 dataset
+results = model.train(data='DOTAv2.yaml', epochs=100, imgsz=640)
+success = model.export(format='tflite')

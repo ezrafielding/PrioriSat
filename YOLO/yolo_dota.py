c0c037305980c618f75379cad9181f521f6f10e3
Ezra Fielding <ezra.fielding@gmail.com>
2024-02-08 18:01:30 +0900

    Update to DOTA1.5

--- YOLO/yolo_dota.py
+++ YOLO/yolo_dota.py
@@ -1,8 +1,6 @@
 from ultralytics import YOLO
 
-# Create a new YOLOv8n-OBB model from scratch
-model = YOLO('yolov8n.yaml')
+model = YOLO('yolov8n-obb.pt')
 
 # Train the model on the DOTAv2 dataset
-results = model.train(data='DOTAv2.yaml', epochs=100, imgsz=320)
-success = model.export(format='tflite')
+results = model.train(data='DOTAv1.5.yaml', epochs=500, imgsz=640)
